be5fad170c940c633dd35d2296cce0406daba8be
Peter Halliday <pghalliday@gmail.com>
2012-11-14 15:36:52 +0100

    starting index

---
+++ test/Source/index.js
@@ -0,0 +1,15 @@
+var expect = require('chai').expect;
+
+describe('index', function() {
+  before(function(done) {
+    done();
+  });
+
+  it('should start an HTTP server', function(done) {
+    done();
+  });
+
+  after(function(done) {
+    done();
+  });
+});
