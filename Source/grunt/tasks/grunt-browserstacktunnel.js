9265cd5997229e842dad66ec721d133a0ebea651
Peter Halliday <pghalliday@gmail.com>
2012-11-20 15:52:43 +0100

    adding task to startup testacular and the browserstack tunnel and the beginning of each development session

---
+++ Source/grunt/tasks/grunt-browserstacktunnel.js
@@ -0,0 +1,26 @@
+/*
+ * grunt-browserstack
+ * https://github.com/pghalliday/grunt-browserstack
+ *
+ * Copyright (c) 2012 Peter Halliday
+ * Licensed under the MIT license.
+ */
+
+var BrowserStackTunnel = require('browserstacktunnel-wrapper');
+
+module.exports = function(grunt) {
+  grunt.registerMultiTask('browserStackTunnel', 'start the browserstack tunnel', function() {
+    var self = this;
+    var done = self.async();
+    var browserStackTunnel = new BrowserStackTunnel(self.data.key, self.data.hosts);
+    browserStackTunnel.start(function(error) {
+      if (error) {
+        done(error);
+      } else {
+        if (!self.data.keepAlive) {
+          done();
+        }
+      }
+    });
+  });
+};
