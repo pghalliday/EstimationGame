e5ec86cbcd8a9e5ac91cf3da171e891108bc5e31
Peter Halliday <pghalliday@gmail.com>
2013-02-12 11:15:47 +0100

    trash

--- Spikes/pressAnyKey.js
+++
@@ -1,8 +0,0 @@
-console.log('press any key');
-
-process.stdin.setRawMode(true);    
-process.stdin.resume();
-function end(chunk) {
-  process.stdin.pause();
-}
-process.stdin.on('data', end);
--- browserStackConfig.example.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  key: 'YOUR_KEY',
-  username: 'YOUR_USERNAME',
-  password: 'YOUR_PASSWORD'
-};
